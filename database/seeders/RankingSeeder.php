eb7ab5e6b1d7162f3555f7e51a960a0572d67175
Mykyta Mykulskyi <81817136+Mik-813@users.noreply.github.com>
2026-01-13 19:49:28 +0100

    update seeder

--- database/seeders/RankingSeeder.php
+++ database/seeders/RankingSeeder.php
@@ -34,22 +34,24 @@ class RankingSeeder extends Seeder
                 'email_verified_at' => now(),
             ]);
 
-            // Create 3 activities for each user
+            $types = ['run', 'bicycle', 'walk'];
+
             for ($i = 1; $i <= 3; $i++) {
-                // Pick a random time within the previous week (0 to 6 days after Monday)
-                $activityDate = $startOfPreviousWeek->copy()->addDays(rand(0, 6))->addHours(rand(6, 20));
+                $activityDate = $startOfPreviousWeek
+                    ->copy()
+                    ->addDays(rand(0, 6))
+                    ->addHours(rand(6, 20));
 
                 $activity = $user->activities()->make([
                     'title' => "{$data['name']}'s Activity #{$i}",
                     'note' => 'Seeded for ranking test',
-                    'activity_type' => 'run',
-                    'distance' => rand(300, 1500) / 100, // Random distance between 3.00 and 15.00 km
+                    'activity_type' => $types[$i - 1],
+                    'distance' => rand(300, 1500) / 100,
                     'time' => rand(1200, 4000),
                     'pace' => 5.30,
                     'speed' => 11.5,
                 ]);
 
-                // Manually set timestamps to ensure they fall in the target week
                 $activity->created_at = $activityDate;
                 $activity->updated_at = $activityDate;
                 $activity->save();
